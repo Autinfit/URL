e4797b2a7b703cc6cae533e8d11ed953efe98696
Marcelo Andrés <69094327+MARSFOREVER472@users.noreply.github.com>
2024-08-02 23:13:56 -0400

    Interfaz creada hasta el momento...

--- Cargar_imagen/Form1.Designer.cs
+++ Cargar_imagen/Form1.Designer.cs
@@ -28,12 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            imagen = new PictureBox();
+            label1 = new Label();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)imagen).BeginInit();
+            SuspendLayout();
+            // 
+            // imagen
+            // 
+            imagen.Location = new Point(12, 12);
+            imagen.Name = "imagen";
+            imagen.Size = new Size(754, 498);
+            imagen.TabIndex = 0;
+            imagen.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(300, 519);
+            label1.Name = "label1";
+            label1.Size = new Size(141, 25);
+            label1.TabIndex = 1;
+            label1.Text = "Imagen de 0 a 0";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(317, 547);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 34);
+            button1.TabIndex = 2;
+            button1.Text = "Modificar";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(778, 744);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Controls.Add(imagen);
+            Name = "Form1";
+            Text = "URL";
+            ((System.ComponentModel.ISupportInitialize)imagen).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private PictureBox imagen;
+        private Label label1;
+        private Button button1;
     }
 }
